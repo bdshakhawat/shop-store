2413a2880176bccc1671fb28151ee5293ffa5610
bdshakhawat <mdshakwat.hossain@gmail.com>
2024-12-01 15:50:51 +0600

    interface commit

--- src/interfaces/ProductInterface.ts
+++ src/interfaces/ProductInterface.ts
@@ -1,1 +1,17 @@
 
+import { Document } from 'mongoose';
+
+export interface Product extends Document {
+  name: string;
+  brand: string;
+  price: number;
+  category:
+    | 'Writing'
+    | 'Office Subpplies'
+    | 'Art Supplies'
+    | 'Educational'
+    | 'Technology';
+  description: string;
+  quantity: number;
+  inStock: boolean;
+}
